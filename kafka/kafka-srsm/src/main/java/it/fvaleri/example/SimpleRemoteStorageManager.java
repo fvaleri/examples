f2f5178f6d6cf0149e97aa64acef707ef744c8d0
Federico Valeri <fedevaleri@gmail.com>
2024-02-05 16:21:56 +0100

    Cleanup
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- kafka/kafka-srsm/src/main/java/it/fvaleri/example/SimpleRemoteStorageManager.java
+++ kafka/kafka-srsm/src/main/java/it/fvaleri/example/SimpleRemoteStorageManager.java
@@ -6,6 +6,8 @@ import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentMetadata;
 import org.apache.kafka.server.log.remote.storage.RemoteResourceNotFoundException;
 import org.apache.kafka.server.log.remote.storage.RemoteStorageException;
 import org.apache.kafka.server.log.remote.storage.RemoteStorageManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -18,40 +20,46 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.lang.String.format;
+
 /**
  * Simple RemoteStorageManager implementation that uses local disk as remote storage.
  */
 public class SimpleRemoteStorageManager implements RemoteStorageManager {
-    public Path tmpdir;
-    
-    private Optional<Path> getIndexFilePath(LogSegmentData logSegmentData, RemoteStorageManager.IndexType indexType) {
-        switch (indexType) {
-            case OFFSET:
-                return Optional.of(logSegmentData.offsetIndex());
-            case PRODUCER_SNAPSHOT:
-                return Optional.of(logSegmentData.producerSnapshotIndex());
-            case TIMESTAMP:
-                return Optional.of(logSegmentData.timeIndex());
-            case TRANSACTION:
-                return logSegmentData.transactionIndex();
-            default:
-                throw new IllegalArgumentException(String.format("index type %s does not have a file path", indexType));
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleRemoteStorageManager.class);
+    private static final Path TMP_DIR = Paths.get("/tmp/remote");
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+        try {
+            if (!Files.exists(TMP_DIR)) {
+                Files.createDirectory(TMP_DIR);
+                LOG.info("Directory created: {}", TMP_DIR);
+            } else {
+                LOG.info("Directory {} already exists", TMP_DIR);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public void close() throws IOException {
     }
     
     @Override
-    public Optional<RemoteLogSegmentMetadata.CustomMetadata>  copyLogSegmentData(RemoteLogSegmentMetadata remoteLogSegmentMetadata, LogSegmentData logSegmentData) throws RemoteStorageException {
-        System.out.println("copyLogSegmentData:" + remoteLogSegmentMetadata + ";;" + tmpdir.toString());
+    public Optional<RemoteLogSegmentMetadata.CustomMetadata> copyLogSegmentData(RemoteLogSegmentMetadata remoteLogSegmentMetadata, LogSegmentData logSegmentData) throws RemoteStorageException {
+        LOG.info("Invoked copyLogSegmentData: {}", remoteLogSegmentMetadata);
         try {
-            Files.copy(logSegmentData.logSegment(), tmpdir.resolve(getBlobName(remoteLogSegmentMetadata, "segment")));
-            System.out.println("copy to " + tmpdir.resolve(getBlobName(remoteLogSegmentMetadata, "segment")));
+            Files.copy(logSegmentData.logSegment(), TMP_DIR.resolve(getBlobName(remoteLogSegmentMetadata, "segment")));
+            LOG.info("Copy to {}", TMP_DIR.resolve(getBlobName(remoteLogSegmentMetadata, "segment")));
             for (IndexType indexType : IndexType.values()) {
                 if (indexType == IndexType.LEADER_EPOCH) {
-                    Files.write(tmpdir.resolve(getBlobName(remoteLogSegmentMetadata, indexType.toString())), logSegmentData.leaderEpochIndex().array());
+                    Files.write(TMP_DIR.resolve(getBlobName(remoteLogSegmentMetadata, indexType.toString())), logSegmentData.leaderEpochIndex().array());
                 } else {
                     Optional<Path> path = getIndexFilePath(logSegmentData, indexType);
                     if (path.isPresent()) {
-                        Files.copy(path.get(), tmpdir.resolve(getBlobName(remoteLogSegmentMetadata, indexType.toString())));
+                        Files.copy(path.get(), TMP_DIR.resolve(getBlobName(remoteLogSegmentMetadata, indexType.toString())));
                     }
                 }
             }
@@ -61,27 +69,18 @@ public class SimpleRemoteStorageManager implements RemoteStorageManager {
         return Optional.empty();
     }
 
-    private String getBlobName(RemoteLogSegmentMetadata remoteLogSegmentMetadata, String suffix) {
-        int partition = remoteLogSegmentMetadata.remoteLogSegmentId().topicIdPartition().topicPartition().partition();
-        Uuid id = remoteLogSegmentMetadata.remoteLogSegmentId().id();
-        String logSegmentId = new UUID(id.getMostSignificantBits(), id.getLeastSignificantBits()).toString();
-        return String.format("%d.%s.%s", partition, logSegmentId, suffix);
-    }
-
     @Override
     public InputStream fetchLogSegment(RemoteLogSegmentMetadata remoteLogSegmentMetadata, int startPosition) throws RemoteStorageException {
         return fetchLogSegment(remoteLogSegmentMetadata, startPosition, Integer.MAX_VALUE);
-
     }
 
     @Override
     public InputStream fetchLogSegment(RemoteLogSegmentMetadata remoteLogSegmentMetadata, int startPosition, int endPosition) throws RemoteStorageException {
-        System.out.println("fetchLogSegment:" + remoteLogSegmentMetadata);
+        LOG.info("Invoked fetchLogSegment: {}", remoteLogSegmentMetadata);
         String name = getBlobName(remoteLogSegmentMetadata, "segment");
-        Path path = tmpdir.resolve(name);
+        Path path = TMP_DIR.resolve(name);
         try {
             byte[] content = Files.readAllBytes(path);
-
             int length = Math.min(content.length - 1, endPosition) - startPosition + 1;
             return new ByteArrayInputStream(content, startPosition, length);
         } catch (IOException e) {
@@ -91,9 +90,9 @@ public class SimpleRemoteStorageManager implements RemoteStorageManager {
 
     @Override
     public InputStream fetchIndex(RemoteLogSegmentMetadata remoteLogSegmentMetadata, IndexType indexType) throws RemoteStorageException {
-        System.out.println("fetchIndex:" + indexType + "; metadata:" + remoteLogSegmentMetadata);
+        LOG.info("Invoked fetchIndex: {}; metadata: {}", indexType, remoteLogSegmentMetadata);
         String name = getBlobName(remoteLogSegmentMetadata, indexType.toString());
-        Path path = tmpdir.resolve(name);
+        Path path = TMP_DIR.resolve(name);
         try {
             byte[] content = Files.readAllBytes(path);
             return new ByteArrayInputStream(content);
@@ -109,14 +108,14 @@ public class SimpleRemoteStorageManager implements RemoteStorageManager {
 
     @Override
     public void deleteLogSegmentData(RemoteLogSegmentMetadata remoteLogSegmentMetadata) throws RemoteStorageException {
-        System.out.println("deleteLogSegmentData:" + remoteLogSegmentMetadata);
+        LOG.info("Invoked deleteLogSegmentData: {}", remoteLogSegmentMetadata);
         String name = getBlobName(remoteLogSegmentMetadata, "segment");
-        Path path = tmpdir.resolve(name);
+        Path path = TMP_DIR.resolve(name);
         try {
             Files.deleteIfExists(path);
             for (IndexType indexType : IndexType.values()) {
                 name = getBlobName(remoteLogSegmentMetadata, indexType.toString());
-                path = tmpdir.resolve(name);
+                path = TMP_DIR.resolve(name);
                 Files.deleteIfExists(path);
             }
         } catch (IOException e) {
@@ -124,23 +123,25 @@ public class SimpleRemoteStorageManager implements RemoteStorageManager {
         }
     }
 
-    @Override
-    public void close() throws IOException {
+    private String getBlobName(RemoteLogSegmentMetadata remoteLogSegmentMetadata, String suffix) {
+        int partition = remoteLogSegmentMetadata.remoteLogSegmentId().topicIdPartition().topicPartition().partition();
+        Uuid id = remoteLogSegmentMetadata.remoteLogSegmentId().id();
+        String logSegmentId = new UUID(id.getMostSignificantBits(), id.getLeastSignificantBits()).toString();
+        return format("%d.%s.%s", partition, logSegmentId, suffix);
     }
 
-    @Override
-    public void configure(Map<String, ?> configs) {
-        try {
-            String fileName = "/tmp/remote";
-            tmpdir = Paths.get(fileName);
-            if (!Files.exists(tmpdir)) {
-                Files.createDirectory(tmpdir);
-                System.out.println("Directory created:" + tmpdir);
-            } else {
-                System.out.println("Directory already exists");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private Optional<Path> getIndexFilePath(LogSegmentData logSegmentData, RemoteStorageManager.IndexType indexType) {
+        switch (indexType) {
+            case OFFSET:
+                return Optional.of(logSegmentData.offsetIndex());
+            case TIMESTAMP:
+                return Optional.of(logSegmentData.timeIndex());
+            case TRANSACTION:
+                return logSegmentData.transactionIndex();
+            case PRODUCER_SNAPSHOT:
+                return Optional.of(logSegmentData.producerSnapshotIndex());
+            default:
+                throw new IllegalArgumentException(format("Index type %s does not have a file path", indexType));
         }
     }
 }
